40e9790c937fe4398fa660a0ef1ae9ed116b9b7f
Riccardo250 <riccardo.lamarca01@gmail.com>
2025-10-28 09:48:15 +0100

    added some pareser structures

---
+++ parser.c
@@ -0,0 +1,8 @@
+#include "parser.h"
+
+//parserGraph
+state parserGraph::closureAction(state& state) {
+    state currState = std::cpy(state);
+
+    //TODO
+}
--- parser.h
+++ parser.h
@@ -5,4 +5,20 @@ class item {
     int dotPosition;
 };
 
+class state {
+    std::vector<item> items;
+};
+
+class edge {
+    state& to;
+    symbol s;
+};
+
+class parseGraph {
+    private:
+        std::vector<std::pair<state, std::vector<edge>>> adjVector;
+        state closureAction(state& state);
+        state gotoAction(state& state, symbol& symbol);
+};
+
 
